aab28e0785697e8d88587a7ab1c971b251c1a57e
Prajwal Ukhalkar <pajup6354@gmail.com>
2026-09-07 16:38:25 +0530

    Add: products-of-array-discluding-self - submission-6

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-6.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+     
+        const res = new Array(nums.length);
+    let prodRight = 1;
+    let prodLeft = 1;
+
+    // use same res array for storing the prefix
+    for(let i=0; i < nums.length; i++) {
+        res[i] = prodLeft;
+        prodLeft = prodLeft * nums[i]
+    }
+
+    
+    for(let i = nums.length - 1; i >= 0; i--) {
+        res[i] = prodRight * res[i];       // Use the suffix product for this index.
+        prodRight = prodRight * nums[i]    // Update suffix product for the next index.
+    }
+        return res;
+    }
+}
